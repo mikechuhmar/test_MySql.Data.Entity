259ce3b1b0ac4ca5f1d04e99b8657fecfedd93be
mikechuhmar <miha1797@gmail.com>
2018-10-06 23:33:44 +0300

    Change Main

--- TestDb/TestDb/Program.cs
+++ TestDb/TestDb/Program.cs
@@ -1,15 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MySql.Data.Entity;
+using MySql.Data.MySqlClient;
+using System;
+using System.Data.Entity;
 
 namespace TestDb
 {
+    // Code-Based Configuration and Dependency resolution
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
     class Program
     {
         static void Main(string[] args)
         {
+            string user_id, password, database;
+            Console.WriteLine("Enter User Id");
+            user_id = Console.ReadLine();
+            Console.WriteLine("Enter Password");
+            password = Console.ReadLine();
+            Console.WriteLine("Enter Dtabase Name");
+            database = Console.ReadLine();
+            string connectionString = "server=localhost;user id=" + user_id + ";password=" + password + ";database=" + database + ";persistsecurityinfo=True";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            try
+            {
+                using (var context = new ProductContext(connection, true))
+                {
+                    context.Database.CreateIfNotExists();
+                    Console.WriteLine("Create A New Product");
+
+                    Product product = new Product();
+                    Console.WriteLine("Enter Price");
+                    product.Price = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Product Name");
+                    product.ProductName = Console.ReadLine();
+                    Console.WriteLine("Enter Quantity");
+                    product.Quantity = int.Parse(Console.ReadLine());
+                    context.Products.Add(product);
+                    if (context.SaveChanges() > 0)
+                    {
+                        Console.WriteLine("New Product Created");
+                        Console.ReadLine();
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error in Creating Product");
+                    }
+
+
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Please enter correct data");
+                Console.ReadKey();
+            }
+
+
+
         }
     }
 }
