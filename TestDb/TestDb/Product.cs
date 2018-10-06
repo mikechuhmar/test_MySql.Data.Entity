70934265af702405957d08a5caedaae1153d8c26
mikechuhmar <miha1797@gmail.com>
2018-10-06 23:24:09 +0300

    Add packages Create class Product

---
+++ TestDb/TestDb/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestDb
+{
+    class Product
+    {
+        [Key]
+        public int ProdictId { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
