8f5d35cc4fdc315dfaeaa82ebd828ac5b0a92310
mikechuhmar <miha1797@gmail.com>
2018-10-06 23:28:12 +0300

    Create class ProductContext

---
+++ TestDb/TestDb/ProductContext.cs
@@ -0,0 +1,22 @@
+﻿using MySql.Data.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDb
+{
+    [DbConfigurationType(typeof(MySqlEFConfiguration))]
+    class ProductContext : DbContext
+    {
+        public virtual DbSet<Product> Products { get; set; }
+        public ProductContext(DbConnection existingConnection, bool contextOwnsConnection) : base(existingConnection, contextOwnsConnection)
+        {
+
+        }
+
+    }
+}
